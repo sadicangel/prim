e10d7f227de5ed37e021105094c171a50b9cd59e
sadicangel <sadicangel@hotmail.com>
2023-06-02 19:54:48 +0100

    Adds Parser_UnaryExpression_HonorsPrecedence tests.

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -4,7 +4,7 @@ namespace Tests;
 public sealed class ParserTests
 {
     [Theory]
-    [MemberData(nameof(GetBinaryOperatorsData))]
+    [MemberData(nameof(GetBinaryOperatorsPairsData))]
     public void Parser_BinaryExpression_HonorsPrecedence(TokenKind op1, TokenKind op2)
     {
         var op1Precedence = op1.GetBinaryOperatorPrecendence();
@@ -46,10 +46,57 @@ public sealed class ParserTests
         }
     }
 
-    public static IEnumerable<object[]> GetBinaryOperatorsData()
+    public static IEnumerable<object[]> GetBinaryOperatorsPairsData()
     {
         foreach (var op1 in SyntaxFacts.GetBinaryOperators())
             foreach (var op2 in SyntaxFacts.GetBinaryOperators())
                 yield return new object[] { op1, op2 };
+    }
+
+
+    [Theory]
+    [MemberData(nameof(GetUnaryOperatorsPairsData))]
+    public void Parser_UnaryExpression_HonorsPrecedence(TokenKind unaryKind, TokenKind binaryKind)
+    {
+        var unaryPrecedence = unaryKind.GetUnaryOperatorPrecendence();
+        var binaryPrecedence = binaryKind.GetBinaryOperatorPrecendence();
+
+        var unaryText = unaryKind.GetText();
+        var binaryText = binaryKind.GetText();
+
+        var text = $"{unaryText} a {binaryText} b";
+        var expr = SyntaxTree.Parse(text).Root;
+
+        if (unaryPrecedence >= binaryPrecedence)
+        {
+            using var e = new AssertingEnumerator(expr);
+            e.AssertNode(NodeKind.BinaryExpression);
+            e.AssertNode(NodeKind.UnaryExpression);
+            e.AssertToken(unaryKind, unaryText!);
+            e.AssertNode(NodeKind.NameExpression);
+            e.AssertToken(TokenKind.Identifier, "a");
+            e.AssertToken(binaryKind, binaryText!);
+            e.AssertNode(NodeKind.NameExpression);
+            e.AssertToken(TokenKind.Identifier, "b");
+        }
+        else
+        {
+            using var e = new AssertingEnumerator(expr);
+            e.AssertNode(NodeKind.UnaryExpression);
+            e.AssertToken(unaryKind, unaryText!);
+            e.AssertNode(NodeKind.BinaryExpression);
+            e.AssertNode(NodeKind.NameExpression);
+            e.AssertToken(TokenKind.Identifier, "a");
+            e.AssertToken(binaryKind, binaryText!);
+            e.AssertNode(NodeKind.NameExpression);
+            e.AssertToken(TokenKind.Identifier, "b");
+        }
+    }
+
+    public static IEnumerable<object[]> GetUnaryOperatorsPairsData()
+    {
+        foreach (var unary in SyntaxFacts.GetUnaryOperators())
+            foreach (var binary in SyntaxFacts.GetBinaryOperators())
+                yield return new object[] { unary, binary };
     }
 }
