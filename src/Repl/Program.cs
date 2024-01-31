ab7121c13573f1e86980affed6a08c4c34a88514
sadicangel <sadicangel@hotmail.com>
2024-01-31 01:00:01 +0000

    Fixes project in wrong path.

---
+++ src/Repl/Program.cs
@@ -0,0 +1,13 @@
+﻿using CodeAnalysis.Syntax;
+using Spectre.Console;
+
+var syntaxTree = SyntaxTree.ParseScript("""
+    for (a : get_range()) {
+        print(a + 2);
+        print("done!");
+    }
+    """);
+
+
+
+AnsiConsole.Write(syntaxTree.ToRenderable());
