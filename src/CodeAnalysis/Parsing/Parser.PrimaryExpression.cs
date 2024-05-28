919e85029ac8a1d50a4e729b60006f682050c8fe
sadicangel <sadicangel@hotmail.com>
2024-05-28 19:43:01 +0100

    Fixes test.

--- src/CodeAnalysis/Parsing/Parser.PrimaryExpression.cs
+++ src/CodeAnalysis/Parsing/Parser.PrimaryExpression.cs
@@ -7,16 +7,15 @@ partial class Parser
     private static ExpressionSyntax ParsePrimaryExpression(SyntaxTree syntaxTree, SyntaxTokenIterator iterator)
     {
         var current = iterator.Current;
-        var next = iterator.Peek(1).SyntaxKind;
         return current.SyntaxKind switch
         {
             >= SyntaxKind.TrueKeyword and <= SyntaxKind.NullKeyword => ParseLiteralExpression(syntaxTree, iterator),
             >= SyntaxKind.I32LiteralToken and <= SyntaxKind.StrLiteralToken => ParseLiteralExpression(syntaxTree, iterator),
             SyntaxKind.ParenthesisOpenToken => ParseGroupExpression(syntaxTree, iterator),
-            _ => next switch
+            _ => iterator.Peek(1).SyntaxKind switch
             {
                 SyntaxKind.ColonToken => ParseLocalDeclaration(syntaxTree, iterator),
-                _ when SyntaxFacts.IsAssignmentOperator(next) => ParseAssignmentExpression(syntaxTree, iterator),
+                SyntaxKind @operator when SyntaxFacts.IsAssignmentOperator(@operator) => ParseAssignmentExpression(syntaxTree, iterator),
                 _ => ParseIdentifierNameExpression(syntaxTree, iterator),
             }
         };
--- tests/CodeAnalysis.Tests/Parsing/ParserTests.VariableDeclaration.cs
+++ tests/CodeAnalysis.Tests/Parsing/ParserTests.VariableDeclaration.cs
@@ -4,7 +4,7 @@ partial class ParserTests
     [Fact]
     public void Parse_VariableDeclaration_Variable()
     {
-        var unit = SyntaxTree.ParseScript(new SourceText($"a: i32 = 2"));
+        var unit = SyntaxTree.Parse(new SourceText($"a: i32 = 2;"));
         var node = Assert.Single(unit.Root.SyntaxNodes);
         Assert.Empty(unit.Diagnostics);
         Assert.Equal(SyntaxKind.VariableDeclaration, node.SyntaxKind);
