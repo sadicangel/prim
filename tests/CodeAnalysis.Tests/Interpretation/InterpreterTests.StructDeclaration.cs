fd9439e898bfca81ef474b8bb39cafd4ed6c0565
sadicangel <sadicangel@hotmail.com>
2024-07-22 23:32:10 +0100

    Adds evaluation tests for struct declaration and initialization.

---
+++ tests/CodeAnalysis.Tests/Interpretation/InterpreterTests.StructDeclaration.cs
@@ -0,0 +1,14 @@
+﻿using CodeAnalysis.Interpretation.Values;
+
+namespace CodeAnalysis.Tests.Interpretation;
+public partial class InterpreterTests
+{
+    [Fact]
+    public void Evaluates_StructDeclaration()
+    {
+        var value = """
+            Point: struct: { x: i32 = 0;  y: i32 = 0; }
+            """.Evaluate();
+        Assert.True(value is StructValue);
+    }
+}
---
+++ tests/CodeAnalysis.Tests/Interpretation/InterpreterTests.StructInitExpression.cs
@@ -0,0 +1,20 @@
+﻿using CodeAnalysis.Interpretation.Values;
+
+namespace CodeAnalysis.Tests.Interpretation;
+public partial class InterpreterTests
+{
+    [Fact]
+    public void Evaluates_StructInitExpression()
+    {
+        var value = """
+            Point: struct: { x: i32 = 0;  y: i32 = 0; }
+            Point { .x = 10, .y = 10 }
+            """.Evaluate();
+        var objectValue = Assert.IsType<ObjectValue>(value);
+        var x = objectValue.Members.Single(m => m.Key.Name == "x").Value.Value;
+        var y = objectValue.Members.Single(m => m.Key.Name == "y").Value.Value;
+
+        Assert.Equal(10, x);
+        Assert.Equal(10, y);
+    }
+}
